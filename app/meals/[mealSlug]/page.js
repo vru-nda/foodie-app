f8cb0201f91b3e4371ebfbb933b6f8091e40d901
Vrunda <vrunda1002@gmail.com>
2024-02-07 13:08:42 +0530

    Meal details page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,43 @@
+import {getMeal} from '@/lib/meals';
+import Image from 'next/image';
+
+import classes from './page.module.css';
+
+import React from 'react';
+import {notFound} from 'next/navigation';
+
 const MealDetailsPage = ({params}) => {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  const instructions = meal.instructions.replace(/\n/g, '<br/>');
+
   return (
-    <main>
-      <h1>{params.mealSlug}</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt='food image' fill priority />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summery}>{meal.summery}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 };
 
---
+++ app/meals/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MealNotFound = () => {
+  return (
+    <main className='not-found'>
+      <h1>Meal Not Found</h1>
+      <p>Unfortunately, we could not found the requested page or meal data.</p>
+    </main>
+  );
+};
+
+export default MealNotFound;
