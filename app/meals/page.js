4d2e0f6b9191ec712ef7aa27ba6354de99ed8e33
Vrunda <vrunda1002@gmail.com>
2024-02-07 12:10:38 +0530

    Fetching data with sqlite and displaying meals

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,40 @@
+import Link from 'next/link';
+import {Suspense} from 'react';
+
+import MealsGrid from '@/components/meals/meals-grid';
+import {getMeals} from '@/lib/meals';
+
+import classes from './page.module.css';
+
+export const Meals = async () => {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+};
+
 const MealsPage = () => {
   return (
-    <main>
-      <h1>MealsPage</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href={'/meals/share'}>Share Your Favourite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals... </p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
+    </>
   );
 };
 
